e0cf2e92a9586860fc011f4e8ecf6e6becd572f7
mkorczynska <k.martyna95@gmail.com>
2021-11-08 23:52:04 +0100

    .

--- login.php
+++ login.php
@@ -1,3 +1,15 @@
 ﻿<?php
-echo "Hello world!"
+
+if(isset($_POST['login'])){
+    echo "Cześć " . htmlspecialchars($_POST['login']) . ". ";
+}else{
+    echo "Cześć nieznajomy.";
+}
+
+if(isset($_POST['password'])){
+    echo "Twoje zaszyfrowane hasło to: ".sha1($_POST['password']);
+}else{
+    echo "Brak hasła.";
+}
+
 ?>
